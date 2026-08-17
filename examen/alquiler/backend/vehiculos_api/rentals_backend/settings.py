3e832529adf2a7d5278ebbda145ad2638069f96d
Melanie <meldaniela.estevez@gmail.com>
2026-08-17 18:12:41 -0500

    readme

--- examen/alquiler/backend/vehiculos_api/rentals_backend/settings.py
+++ examen/alquiler/backend/vehiculos_api/rentals_backend/settings.py
@@ -98,6 +98,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     os.getenv("CORS_ORIGIN", "http://localhost:5173"),
     "http://127.0.0.1:5173",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
 ]
 
 # Mongo settings
